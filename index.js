80cbd2c2a6e08e19034d3380cadba4e6573c59cc
Michal Puchmertl <puchmertl.michal12@gmail.com>
2021-08-06 03:52:50 +0000

    init commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+var crypto = require('crypto');
+
+app.get('/get-stream-url', (req, res) => {
+  const ip = req.ip.match(/[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/gm);
+
+  res.send(getStreamUrl(ip[0], 'uigfp(@#tfpIUDGPFiouGDF'));
+});
+
+function generateSecurePathHash(expires, client_ip, secret) {
+  if (!expires || !client_ip || !secret)
+    throw new Error('Must provide all token components');
+
+  var input = expires + ' ' + client_ip + ' ' + secret;
+  var binaryHash = crypto.createHash('md5').update(input).digest();
+  var base64Value = new Buffer(binaryHash).toString('base64');
+  return base64Value.replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_');
+}
+
+function getStreamUrl(ip, secret) {
+  const expiresTimestamp = new Date(Date.now() + 1000 * 60 * 30).getTime();
+  const expires = String(Math.round(expiresTimestamp / 1000));
+
+  const token = generateSecurePathHash(expires, ip, secret);
+
+  return `https://example.com/video/hls/${token}/${expires}/play.m3u8`;
+}
+
+getStreamUrl('127.0.0.1', 'uigfp(@#tfpIUDGPFiouGDF');
+
+app.listen(port, () => {
+  console.log(`Success! Stream link issuer is listening on port ${port}!`);
+});
